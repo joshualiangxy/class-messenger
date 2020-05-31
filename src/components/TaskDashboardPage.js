9462d3235da076dc9e201afcdae89853495c3dfd
Joshua Liang XingYa <joshualiang.xy@gmail.com>
2020-05-31 13:49:21 +0800

    Remove logout button from TaskDashboardPage

--- src/components/TaskDashboardPage.js
+++ src/components/TaskDashboardPage.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { startLogout } from '../actions/auth';
 
-export const TaskDashboardPage = ({ startLogout }) => (
+export const TaskDashboardPage = () => (
   <div>
     <h1>This is the task dashboard page</h1>
-    <button onClick={startLogout}>Logout</button>
   </div>
 );
 
-const mapDispatchToProps = dispatch => ({
-  startLogout: () => dispatch(startLogout())
-});
+const mapDispatchToProps = dispatch => ({});
 
 export default connect(undefined, mapDispatchToProps)(TaskDashboardPage);
