522105ad2b32da7d146475c1cb14030dbee938b9
Jazer Ler <59879284+jazerler@users.noreply.github.com>
2020-06-12 15:53:54 +0800

    Move addNewUser to actions and add more fields to startNewGroup action
    startNewGroup only put the name, uid, and admin status of a user when they started a group, which wasn't consistent with the field names of addNewUser's updates.

--- src/actions/groups.js
+++ src/actions/groups.js
@@ -27,29 +27,61 @@ export const startNewGroup = groupName => {
       });
     const groupUserPromise = groupRef.collection('users').doc(uid).set({
       uid: uid,
-      name: getState().user.displayName,
+      displayName: getState().user.displayName,
+      studentNum: getState().user.studentNum,
       admin: true
     });
     return Promise.all([userPromise, groupPromise, groupUserPromise]);
   };
 };
 
-export const addNewUser = (userEmail, group) => {
-  const uid = userEmail; // Change this when email-uid are linked!
-  const groupRef = firebase.firestore().collection('groups').doc(group);
-  const userRef = firebase.firestore().collection('users').doc(uid);
-  if (userRef.exists) {
-    // Create a new document for this user. Non-admin by default, implement promotion later
-    const userProperties = {};
-    userRef
-      .get()
-      .then(snapshot => {
-        userProperties.name = snapshot.displayName;
-        userProperties.sNum = snapshot.studentNum;
-      })
-      .catch(() => {
-        console.log('caught');
-      });
-  }
-};
+export const addNewUser = (userEmail, group, setError) => {
+  firebase
+    .firestore()
+    .collection('emailToUid')
+    .doc(userEmail)
+    .get()
+    .then(uidDoc => {
+      const uidData = uidDoc.data();
+      if (typeof uidData === 'undefined') {
+        // This data doesn't exist / Invalid user
+        setError('No user found');
+      } else {
+        // Both of these references are DocumentReference
+        const userRef = firebase
+          .firestore()
+          .collection('users')
+          .doc(uidData.uid);
+        const groupRef = firebase.firestore().collection('groups').doc(group);
+        const uid = uidData.uid;
 
+        // Gets the data from a user.
+        userRef.get().then(doc => {
+          const data = doc.data();
+          // Write displayName, studentNum, and UID into a new document
+          if (typeof data === 'undefined') {
+            // Most likely a user that doesn't exist.
+            // Deprecated, since there's already the check in emailToUid. 
+            setError('No such user found');
+          } else {
+            groupRef
+              .collection('users')
+              .doc(uid) // Create document for this user's UID
+              .set({
+                studentNum: data.studentNum,
+                displayName: data.displayName,
+                uid: uid,
+                admin: false
+              })
+              .then(() => {
+                setError('User added!');
+              })
+              .catch(error => {
+                setError('Something went wrong.');
+              });
+          }
+        });
+      }
+      // Defaults to non-admin.
+    });
+};
--- src/components/AddUserModal.js
+++ src/components/AddUserModal.js
@@ -1,44 +1,11 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import firebase from '../firebase/firebase';
+import { connect } from 'react-redux';
+import {addNewUser} from '../actions/groups';
 
 // Note: This still works off uid for now. This also does NOT work yet.
 const AddUserModal = ({ isOpen, onRequestClose, group }) => {
-  const addNewUser = (userEmail, group) => {
-    const uid = userEmail; // TODO: Change this when email-uid are linked!
-
-    // Both of these references are DocumentReference
-    const userRef = firebase.firestore().collection('users').doc(uid);
-    const groupRef = firebase.firestore().collection('groups').doc(group);
-
-    // Gets the data from a user.
-    userRef.get().then(doc => {
-      const data = doc.data();
-      // Write displayName, studentNum, and UID into a new document
-      if (typeof data === 'undefined') {
-        // Most likely a user that doesn't exist.
-        setError('No such user found');
-      } else {
-        groupRef
-          .collection('users')
-          .doc(uid) // Create document for this user's UID
-          .set({
-            studentNum: data.studentNum,
-            displayName: data.displayName,
-            uid: uid,
-            admin: false
-          })
-          .then(() => {
-            setError('User added!');
-          })
-          .catch(error => {
-            setError('Something went wrong.');
-          });
-      }
-      // Defaults to non-admin.
-    });
-  };
-
   // TODO: When calling this modal, pass in the group UID as a prop
   const [userEmail, setUserEmail] = useState('');
   const [error, setError] = useState('');
@@ -59,12 +26,7 @@ const AddUserModal = ({ isOpen, onRequestClose, group }) => {
     if (!submittedEmail) {
       setError('Please enter a group name');
     } else {
-      addNewUser(submittedEmail, group);
-      // TODO: Catch user not found
-      // Does not close after submitting, so that the user can continue to add more users
-
-      // setUserEmail(''); // Also resets the field.
-      // setError('Added!');
+      addNewUser(submittedEmail, group, setError);
     }
   };
 
@@ -90,5 +52,9 @@ const AddUserModal = ({ isOpen, onRequestClose, group }) => {
     </Modal>
   );
 };
+
+const mapDispatchToProps = dispatch => ({
+  addNewUser: (userEmail, group, setError) => dispatch(addNewUser(userEmail, group, setError))
+})
 
 export default AddUserModal;
