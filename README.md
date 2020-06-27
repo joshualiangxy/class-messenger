49c15cb70080e1601b1807052e750eb8ade88ac0
Joshua Liang XingYa <joshualiang.xy@gmail.com>
2020-06-27 20:40:55 +0800

    update readme

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This is team stuff's project for Orbital 2020
 
 The webapp is built on ReactJS and redux, and tested using jest.
 
-You can view the tests [here](https://rawcdn.githack.com/joshualiangxy/class-board-tasks/14af1b5c38c1839f3d1aee87999af31f4b99c1bd/jest_html_reporters.html)
+You can view the tests [here](https://raw.githack.com/joshualiangxy/class-board-tasks/master/jest_html_reporters.html)
 
 You can visit the site [here](https://class-board-tasks.herokuapp.com/)
--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,41 +1,41 @@
-//import {
-//sortByDeadline,
-//sortByDeadlineReversed,
-//sortByName,
-//sortByNameReversed,
-//setTextFilter,
-//toggleGrouped
-//} from '../../actions/filters';
+import {
+  sortByDeadline,
+  sortByDeadlineReversed,
+  sortByName,
+  sortByNameReversed,
+  setTextFilter,
+  toggleGrouped
+} from '../../actions/filters';
 
-//describe('sort by deadline', () => {
-//it('should generate action object', () =>
-//expect(sortByDeadline()).toEqual({ type: 'SORT_BY_DEADLINE' }));
+describe('sort by deadline', () => {
+  it('should generate action object', () =>
+    expect(sortByDeadline()).toEqual({ type: 'SORT_BY_DEADLINE' }));
 
-//it('should generate reversed action object', () =>
-//expect(sortByDeadlineReversed()).toEqual({
-//type: 'SORT_BY_DEADLINE_REVERSED'
-//}));
-//});
+  it('should generate reversed action object', () =>
+    expect(sortByDeadlineReversed()).toEqual({
+      type: 'SORT_BY_DEADLINE_REVERSED'
+    }));
+});
 
-//describe('sort by name', () => {
-//it('should generate sort by name action object', () =>
-//expect(sortByName()).toEqual({ type: 'SORT_BY_NAME' }));
+describe('sort by name', () => {
+  it('should generate sort by name action object', () =>
+    expect(sortByName()).toEqual({ type: 'SORT_BY_NAME' }));
 
-//it('should generate reversed action object', () =>
-//expect(sortByNameReversed()).toEqual({ type: 'SORT_BY_NAME_REVERSED' }));
-//});
+  it('should generate reversed action object', () =>
+    expect(sortByNameReversed()).toEqual({ type: 'SORT_BY_NAME_REVERSED' }));
+});
 
-//describe('set text filter', () => {
-//it('should generate set text filter action object with appropriate text', () => {
-//const text = 'abc123';
-//expect(setTextFilter(text)).toEqual({ type: 'SET_TEXT_FILTER', text });
-//});
+describe('set text filter', () => {
+  it('should generate set text filter action object with appropriate text', () => {
+    const text = 'abc123';
+    expect(setTextFilter(text)).toEqual({ type: 'SET_TEXT_FILTER', text });
+  });
 
-//it('should generate set text filter action object with empty string', () =>
-//expect(setTextFilter()).toEqual({ type: 'SET_TEXT_FILTER', text: '' }));
-//});
+  it('should generate set text filter action object with empty string', () =>
+    expect(setTextFilter()).toEqual({ type: 'SET_TEXT_FILTER', text: '' }));
+});
 
-//describe('toggle grouped', () => {
-//it('should generate action object', () =>
-//expect(toggleGrouped()).toEqual({ type: 'TOGGLE_GROUPED' }));
-//});
+describe('toggle grouped', () => {
+  it('should generate action object', () =>
+    expect(toggleGrouped()).toEqual({ type: 'TOGGLE_GROUPED' }));
+});
