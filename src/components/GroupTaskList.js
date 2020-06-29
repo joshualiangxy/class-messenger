823b44fbfce8d00ced1b30963e889b66d7e8b010
Joshua Liang XingYa <joshualiang.xy@gmail.com>
2020-06-29 20:01:33 +0800

    update GroupTaskList to handle updated selector

--- src/components/GroupTaskList.js
+++ src/components/GroupTaskList.js
@@ -19,7 +19,7 @@ const GroupTaskList = ({
 
   return (
     <div>
-      {Object.keys(sortedList).length === 0 ? (
+      {sortedList.others.tasks.length === 0 ? (
         <h3>No tasks yet</h3>
       ) : (
         Object.values(sortedList)[0].tasks.map(task => (
