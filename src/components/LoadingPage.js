2788c364805352217b6712166f174a437cd1fa57
Joshua Liang XingYa <joshualiang.xy@gmail.com>
2020-07-07 19:04:40 +0800

    Update loading gif with proper background

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingPage = () => (
   <div className="box-layout">
     <div className="loader">
-      <img className="loader__image" src="/images/loader.gif" />
+      <img className="loader__image" src="/images/loader.svg" />
       <p className="loader__text">
         This loading gif is provided by{' '}
         <a href="https://loading.io/">loading.io</a>
