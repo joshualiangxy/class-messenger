4a0ae7ae2b501ec84003b963130fe50acf1f692c
Joshua Liang XingYa <joshualiang.xy@gmail.com>
2020-07-10 17:29:35 +0800

    remove box layout from loading page

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,18 +1,12 @@
 import React from 'react';
 
 const LoadingPage = () => (
-  <div className="box-layout">
-    <div className="loader">
-      <img
-        className="loader__image"
-        src="/images/loader.svg"
-        alt="Loading..."
-      />
-      <p className="loader__text">
-        This loading gif is provided by{' '}
-        <a href="https://loading.io/">loading.io</a>
-      </p>
-    </div>
+  <div className="loader">
+    <img className="loader__image" src="/images/loader.svg" alt="Loading..." />
+    <p className="loader__text">
+      This loading gif is provided by{' '}
+      <a href="https://loading.io/">loading.io</a>
+    </p>
   </div>
 );
 
